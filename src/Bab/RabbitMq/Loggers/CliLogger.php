83b78d014ca51dc5b3134e4545ddd4bf23da04a8
Aymeric PLANCHE <planche@domisys.com>
2015-01-13 16:27:43 +0100

    fix: CliLogger could extends AbstractLogger instead of define all interface method

--- src/Bab/RabbitMq/Loggers/CliLogger.php
+++ src/Bab/RabbitMq/Loggers/CliLogger.php
@@ -1,10 +1,10 @@
 <?php
 namespace Bab\RabbitMq\Loggers;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\AbstractLogger;
 
-class CliLogger implements LoggerInterface
+class CliLogger extends AbstractLogger
 {
     private
         $output;
@@ -14,46 +14,6 @@ class CliLogger implements LoggerInterface
         $this->output = $output;
     }
     
-    public function emergency($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function alert($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function critical($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function error($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function warning($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function notice($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function info($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
-    public function debug($message, array $context = array())
-    {
-        $this->log(null, $message);
-    }
-
     public function log($level, $message, array $context = array())
     {
         $this->output->writeln($message);
