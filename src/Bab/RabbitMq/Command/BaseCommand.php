c2bddc3eb7a6d0f7950a98126276237f4425c090
Aymeric PLANCHE <planche@domisys.com>
2015-01-14 12:38:27 +0100

    feat: inject GuzzleClient as httpClient provider

--- src/Bab/RabbitMq/Command/BaseCommand.php
+++ src/Bab/RabbitMq/Command/BaseCommand.php
@@ -8,9 +8,9 @@ use Bab\RabbitMq\VhostManager;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Bab\RabbitMq\Action\RealAction;
-use Bab\RabbitMq\HttpClient\CurlClient;
 use Bab\RabbitMq\Logger\CliLogger;
 use Bab\RabbitMq\Action\DryRunAction;
+use Bab\RabbitMq\HttpClient\GuzzleClient;
 
 class BaseCommand extends Command
 {
@@ -18,6 +18,7 @@ class BaseCommand extends Command
     {
         $this
             ->addOption('host', 'H', InputOption::VALUE_REQUIRED, 'Which host?', '127.0.0.1')
+            ->addOption('scheme', 's', InputOption::VALUE_OPTIONAL, 'Which protocol scheme ? (http(s))')
             ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'Which user?', 'guest')
             ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'Which password? If nothing provided, password is asked', null)
             ->addOption('port', null, InputOption::VALUE_REQUIRED, 'Which port?', 15672)
@@ -37,16 +38,16 @@ class BaseCommand extends Command
     protected function getVhostManager(InputInterface $input, OutputInterface $output, $vhost)
     {
         $context = array(
-            'host'  => $input->getOption('host'),
-            'user'  => $input->getOption('user'),
-            'pass'  => $this->getPassword($input, $output),
-            'port'  => $input->getOption('port'),
-            'vhost' => $vhost
+            'host'   => $input->getOption('host'),
+            'scheme' => $input->getOption('scheme'),
+            'user'   => $input->getOption('user'),
+            'pass'   => $this->getPassword($input, $output),
+            'port'   => $input->getOption('port'),
+            'vhost'  => $vhost
         );
 
         $logger = new CliLogger($output);
-
-        $httpClient = new CurlClient($context['host'], $context['port'], $context['user'], $context['pass']);
+        $httpClient = new GuzzleClient($context['scheme'], $context['host'], $context['port'], $context['user'], $context['pass']);
         
         if ($input->getOption('dry-run'))
         {
